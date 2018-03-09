d01f60e65440015c04a4064aa5186d0e992a178b
Churn2Burn <tylermsilver@gmail.com>
2018-03-09 10:56:29 -0500

    Regex Search for Redeem Button
    Standardize search among email providers. Fixes hotmail bug.

--- master-extractor.py
+++ master-extractor.py
@@ -70,7 +70,7 @@ if status == "OK":
                 msg_parsed = BeautifulSoup(msg_html, 'html.parser')
 
                 # Find the "View Gift" link
-                egc_link = msg_parsed.find("a", text="View My Code") or msg_parsed.find("a", text="Unwrap Your Gift")
+                egc_link = msg_parsed.find("a", text=re.compile("(?i)view\s*my\s*code")) or msg_parsed.find("a", text=re.compile("(?i)unwrap\s*your\s*gift"))
                 if egc_link is not None:
                     # Open the link in the browser
                     browser.get(egc_link['href'])
